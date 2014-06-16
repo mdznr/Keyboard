bea27b91b67a9c0b260d384cf5a3bf0ec43efbb2
Matt Zanchelli <matt@mdznr.com>
2014-06-16 05:06:31 -0400

    Ability to switch keyboard appearance and dismiss keyboard in the example app.

---
+++ Keyboard/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Keyboard
+//
+//  Created by Matt Zanchelli on 6/16/14.
+//  Copyright (c) 2014 Matt Zanchelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet var textField : UITextField
+	
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+	
+	init(coder aDecoder: NSCoder!)  {
+		super.init(coder: aDecoder)
+		// Custom initialization
+	}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+		textField.becomeFirstResponder()
+    }
+
+	@IBAction func didDismissKeyboard(sender : AnyObject) {
+		textField.resignFirstResponder()
+	}
+	
+	@IBAction func didChangeKeyboardAppearance(sender : UISegmentedControl) {
+		switch sender.selectedSegmentIndex {
+		case 2: // .Dark
+			textField.keyboardAppearance = .Dark
+		case 1: // .Light
+			textField.keyboardAppearance = .Light
+		case 0: // .Default
+			textField.keyboardAppearance = .Default
+		default:
+			textField.keyboardAppearance = .Default
+		}
+	}
+}
