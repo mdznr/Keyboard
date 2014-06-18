b6f6c7eec404bb1b3b8bdc1d214bb7fbdccf12d3
Matt Zanchelli <matt@mdznr.com>
2014-06-17 20:04:05 -0400

    A text view instead of a text field in demo app.

--- Keyboard/ViewController.swift
+++ Keyboard/ViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-	@IBOutlet var textField : UITextField
+	@IBOutlet var textView: UITextView
 	
     init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -20,28 +20,96 @@ class ViewController: UIViewController {
 	init(coder aDecoder: NSCoder!)  {
 		super.init(coder: aDecoder)
 		// Custom initialization
+		
+		registerForKeyboardNotifications()
 	}
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+		
+		resetTextViewInsets()
     }
+	
+	func registerForKeyboardNotifications() {
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+		NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+//		NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillChangeFrame:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+	}
+	
+	// Called when `UIKeyboardWillShowNotification` is sent.
+	func keyboardWillShow(aNotification: NSNotification) {
+		let sizeBegin = aNotification.userInfo.objectForKey(UIKeyboardFrameBeginUserInfoKey).CGRectValue().size
+		let sizeEnd = aNotification.userInfo.objectForKey(UIKeyboardFrameEndUserInfoKey).CGRectValue().size
+		
+		let duration = aNotification.userInfo.objectForKey(UIKeyboardAnimationDurationUserInfoKey).doubleValue
+		let curve = aNotification.userInfo.objectForKey(UIKeyboardAnimationCurveUserInfoKey).integerValue
+		
+		
+		var animationCurve: UIViewAnimationCurve
+		if let value = UIViewAnimationCurve.fromRaw(curve) {
+			animationCurve = value
+		} else {
+			animationCurve = UIViewAnimationCurve.EaseInOut
+		}
+		
+		let insets = UIEdgeInsets(top: 44, left: 0, bottom: sizeEnd.height, right: 0)
+		
+		UIView.animateWithDuration(duration, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+			self.textView.contentInset = insets
+			self.textView.scrollIndicatorInsets = insets
+		}, completion: nil)
+	}
+	
+	// Called when `UIKeyboardWillHideNotification` is sent.
+	func keyboardWillHide(aNotification: NSNotification) {
+		let sizeBegin = aNotification.userInfo.objectForKey(UIKeyboardFrameBeginUserInfoKey).CGRectValue().size
+		let sizeEnd = aNotification.userInfo.objectForKey(UIKeyboardFrameEndUserInfoKey).CGRectValue().size
+		
+		let duration = aNotification.userInfo.objectForKey(UIKeyboardAnimationDurationUserInfoKey).doubleValue
+		let curve = aNotification.userInfo.objectForKey(UIKeyboardAnimationCurveUserInfoKey).integerValue
+		
+		
+		var animationCurve: UIViewAnimationCurve
+		if let value = UIViewAnimationCurve.fromRaw(curve) {
+			animationCurve = value
+		} else {
+			animationCurve = UIViewAnimationCurve.EaseInOut
+		}
+		
+		let insets = UIEdgeInsets(top: 44, left: 0, bottom: sizeEnd.height, right: 0)
+		
+		UIView.animateWithDuration(duration, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: {
+			self.textView.contentInset = insets
+			self.textView.scrollIndicatorInsets = insets
+		}, completion: nil)
+	}
+	
+	func resetTextViewInsets() {
+		let contentInsets = UIEdgeInsets(top: 44, left: 0, bottom: 0, right: 0);
+		textView.contentInset = contentInsets;
+		textView.scrollIndicatorInsets = contentInsets;
+	}
+	
+	override func prefersStatusBarHidden() -> Bool {
+		return true
+	}
 
-	@IBAction func didDismissKeyboard(sender : AnyObject) {
-		textField.resignFirstResponder()
+	@IBAction func done(sender: AnyObject) {
+		textView.resignFirstResponder()
 	}
 	
 	@IBAction func didChangeKeyboardAppearance(sender : UISegmentedControl) {
 		switch sender.selectedSegmentIndex {
 		case 2: // .Dark
-			textField.keyboardAppearance = .Dark
+			textView.keyboardAppearance = .Dark
 		case 1: // .Light
-			textField.keyboardAppearance = .Light
+			textView.keyboardAppearance = .Light
 		case 0: // .Default
-			textField.keyboardAppearance = .Default
+			textView.keyboardAppearance = .Default
 		default:
-			textField.keyboardAppearance = .Default
+			textView.keyboardAppearance = .Default
 		}
 	}
 }
